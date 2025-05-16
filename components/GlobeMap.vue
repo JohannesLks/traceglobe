0865fe4a2dd2805c7f8676fa8737a316da668057
Lukas Johannes Möller <74617774+JohannesLks@users.noreply.github.com>
2025-05-16 20:19:20 +0200

    Fix Globe Map Component

--- components/GlobeMap.vue
+++ components/GlobeMap.vue
@@ -1,26 +1,56 @@
 <template>
-  <div id="globeViz" style="height: 600px;"></div>
+  <div id="globeViz" ref="globeContainer" style="height: 600px;"></div>
 </template>
 
 <script setup>
-import { onMounted, watch } from 'vue'
+import { onMounted, onUnmounted, watch, ref } from 'vue'
 import Globe from 'globe.gl'
 
 const props = defineProps({
   points: Array
 })
 
-watch(() => props.points, drawGlobe, { immediate: true })
+const globeContainer = ref(null)
+let globe = null
 
-function drawGlobe() {
-  const globe = Globe()(document.getElementById('globeViz'))
+onMounted(() => {
+  initGlobe()
+  window.addEventListener('resize', handleResize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize)
+  if (globe) {
+    globe._destructor()
+  }
+})
+
+function handleResize() {
+  if (globe && globeContainer.value) {
+    globe.width(globeContainer.value.clientWidth)
+    globe.height(globeContainer.value.clientHeight)
+  }
+}
+
+function initGlobe() {
+  if (!globeContainer.value) return
+  
+  globe = Globe()(globeContainer.value)
   globe
     .pointOfView({ lat: 20, lng: 0, altitude: 2 })
-    .pointsData(props.points)
+    .pointsData(props.points || [])
     .pointLabel('label')
     .pointLat('lat')
     .pointLng('lng')
     .pointColor(() => 'rgba(0,200,255,0.8)')
     .pointAltitude(0.01)
+    .width(globeContainer.value.clientWidth)
+    .height(globeContainer.value.clientHeight)
 }
+
+watch(() => props.points, (newPoints) => {
+  if (globe) {
+    globe.pointsData(newPoints || [])
+  }
+}, { deep: true })
 </script>
