c8fd62d8bea99d34d73dd40d6ba110975dec8861
Lukas Johannes Möller <74617774+JohannesLks@users.noreply.github.com>
2025-05-16 22:12:39 +0200

    Setup deployment configuration

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,86 +1,18 @@
 export default {
-  "_BqYPaJjL.js": {
-    "resourceType": "script",
-    "module": true,
+  "@vite/client": {
     "prefetch": true,
-    "preload": true,
-    "file": "BqYPaJjL.js",
-    "name": "v3",
-    "imports": [
-      "node_modules/nuxt/dist/app/entry.js"
-    ]
-  },
-  "node_modules/nuxt/dist/app/components/error-404.vue": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "file": "DqOkU7kf.js",
-    "name": "error-404",
-    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
-    "isDynamicEntry": true,
-    "imports": [
-      "node_modules/nuxt/dist/app/entry.js",
-      "_BqYPaJjL.js"
-    ],
-    "css": [
-      "error-404.4oxyXxx0.css"
-    ]
-  },
-  "error-404.4oxyXxx0.css": {
-    "file": "error-404.4oxyXxx0.css",
-    "resourceType": "style",
-    "prefetch": true,
-    "preload": true
-  },
-  "node_modules/nuxt/dist/app/components/error-500.vue": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "file": "BSdH83Wv.js",
-    "name": "error-500",
-    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
-    "isDynamicEntry": true,
-    "imports": [
-      "node_modules/nuxt/dist/app/entry.js",
-      "_BqYPaJjL.js"
-    ],
-    "css": [
-      "error-500.CZqNkBuR.css"
-    ]
-  },
-  "error-500.CZqNkBuR.css": {
-    "file": "error-500.CZqNkBuR.css",
-    "resourceType": "style",
-    "prefetch": true,
-    "preload": true
-  },
-  "node_modules/nuxt/dist/app/entry.js": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "file": "C53Z4wb2.js",
-    "name": "entry",
-    "src": "node_modules/nuxt/dist/app/entry.js",
     "isEntry": true,
-    "dynamicImports": [
-      "node_modules/nuxt/dist/app/components/error-404.vue",
-      "node_modules/nuxt/dist/app/components/error-500.vue"
-    ]
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
   },
-  "pages/index.vue": {
+  "/home/project/node_modules/nuxt/dist/app/entry.js": {
     "resourceType": "script",
     "module": true,
     "prefetch": true,
     "preload": true,
-    "file": "DfXcP8c7.js",
-    "name": "index",
-    "src": "pages/index.vue",
-    "isDynamicEntry": true,
-    "imports": [
-      "node_modules/nuxt/dist/app/entry.js"
-    ]
+    "isEntry": true,
+    "file": "/home/project/node_modules/nuxt/dist/app/entry.js"
   }
 }
